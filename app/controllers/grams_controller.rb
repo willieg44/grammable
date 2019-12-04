6246c65b3c28e8d9efe7e05e8cb5c0981b08e4a3
willieg44@gmail.com <willieg44@gmail.com>
2019-12-04 00:12:03 +0000

    added gram to our database

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,5 +1,21 @@
 class GramsController < ApplicationController
+    def new
+        @gram = Gram.new
+    end
+
+
     def index
 
+    end
+
+    def create
+      @gram = Gram.create(grams_params)  
+      redirect_to root_path
+    end
+
+    private
+
+    def grams_params
+        params.require(:gram).permit(:message)
     end
 end
